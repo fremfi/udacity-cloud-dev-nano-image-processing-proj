3d45d3836885d348455f19ae19adfe174dcfec1c
Freddie <frederick@outschool.com>
2024-12-29 00:25:32 -0500

    udacity-cloud-dev-nano-image-processing-proj

--- project starter code/server.js
+++ project starter code/server.js
@@ -1,36 +1,36 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import { query, validationResult } from 'express-validator';
 import {filterImageFromURL, deleteLocalFiles} from './util/util.js';
 
+// Init the Express application
+const app = express();
 
+// Set the network port
+const port = process.env.PORT || 8082;
 
-  // Init the Express application
-  const app = express();
+// Use the body parser middleware for post requests
+app.use(bodyParser.json());
 
-  // Set the network port
-  const port = process.env.PORT || 8082;
-  
-  // Use the body parser middleware for post requests
-  app.use(bodyParser.json());
+app.get( "/filteredimage", query('image_url').notEmpty().isURL({protocols: ['http','https','ftp']}), async (req, res) => {
+    const reqValidationResults = validationResult(req);
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+  if (reqValidationResults?.errors?.length > 0) {
+    return res.status(400).send(`Bad Request: ${reqValidationResults.errors[0].msg}`);
+  }
 
-    /**************************************************************************** */
+  filterImageFromURL(req.query.image_url).then((resolve) => {
 
-  //! END @TODO1
-  
+    return res.status(200).sendFile(resolve)
+  }).catch((error) => {
+    return res.status(500).send(`Internal Server Error: ${error}`);
+  }).finally((resolve) => {
+    if (resolve) {
+      deleteLocalFiles([resolve]);
+    }
+  });
+});
+
   // Root Endpoint
   // Displays a simple message to the user
   app.get( "/", async (req, res) => {
